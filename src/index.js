ad74d992cd8f823cfc95a030acd1aaa5f47db6bc
Ollie Crook <olliejcrook@gmail.com>
2018-06-15 18:24:56 +0100

    add countries searchability, grid layout at med width and sidebar

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ function submitHandler(event) {
     const getTopic = document.querySelector("#topicSearchBar");
     const topicToSearch = getTopic.value;
 
+    const country = document.querySelector("#countries");
+    const countryToSearch = country.value;
+
     // Get news information
-    const url = `https://newsapi.org/v2/everything?q=${topicToSearch}&sortBy=publishedAt&apiKey=814db06ca1814aeca4562dbafab25378`;
+    
+    const url = `https://newsapi.org/v2/top-headlines?country=${countryToSearch}&category=${topicToSearch}&apiKey=814db06ca1814aeca4562dbafab25378`;
+
     fetch(url)
     .then(function(response){
         return response.json();
     }).then(function(data){
-
-        console.log(data);
 
         const results = data.articles.map(article =>
             `<div class="app__content__body__article"><a href="${article.url}"><h2>${article.title}</h2></a><img class="app__content___body__article--img" src="${article.urlToImage}"><h3>${article.source.name}</h3><p>${article.description}</p><p>Author: ${article.author}</p></div>
