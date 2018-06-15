af72ee80343f25b39f58fb2dac5683d53299fbe7
Ollie Crook <olliejcrook@gmail.com>
2018-06-15 15:21:54 +0100

    add sticky footer and img responsiveness

---
+++ src/index.js
@@ -0,0 +1,34 @@
+function submitHandler(event) {
+    event.preventDefault();
+
+    // Capture search term
+    const getTopic = document.querySelector("#topicSearchBar");
+    const topicToSearch = getTopic.value;
+
+    // Get news information
+    const url = `https://newsapi.org/v2/everything?q=${topicToSearch}&sortBy=publishedAt&apiKey=814db06ca1814aeca4562dbafab25378`;
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+
+        console.log(data);
+
+        const results = data.articles.map(article =>
+            `<div class="app__content__body__article"><a href="${article.url}"><h2>${article.title}</h2></a><img class="app__content___body__article--img" src="${article.urlToImage}"><h3>${article.source.name}</h3><p>${article.description}</p><p>Author: ${article.author}</p></div>
+            `
+        ).join('');
+        getTopic.value = '';
+
+        const resultsEl = document.querySelector('#articlesBody');
+
+        resultsEl.innerHTML = results;
+
+    }).catch(function(error){
+        alert('Sorry, I couldn\'t find anything.');
+    });
+}
+
+const getSubmit = document.querySelector("#searchTopic");
+
+getSubmit.addEventListener("submit", submitHandler);
