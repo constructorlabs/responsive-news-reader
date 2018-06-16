4fa2ab3ee84175886cb06a1e2ad29c27c40acb78
Ralph <ralph@rgalan.es>
2018-06-16 16:58:47 +0100

    Added read later feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
-const newsAPI = "8483a39eb2d649bb80b09b3c36cb30df";
-let newsCollection = "";
-const newsDisplay = document.querySelector("#app__content-body--news-display");
-
+const newsAPIKey = "8483a39eb2d649bb80b09b3c36cb30df";
+const newsDisplay = document.querySelector("#app__content-body--news");
+const readLater = [];
+const readLaterButton = document.querySelector("#app__header-nav--readlater");
+let liveNewsCollection = "";
 // Main navigation items
 const mainNav = document.querySelector("#app__header-nav--wrapper");
 const liveNewsButton = document.querySelector("#app__header-nav--live");
 
 function createRequest(service) {
   return service === "live"
-    ? `https://newsapi.org/v2/top-headlines?language=en&apiKey=${newsAPI}`
+    ? `https://newsapi.org/v2/top-headlines?language=en&apiKey=${newsAPIKey}`
     : ``;
 }
 
@@ -17,29 +18,24 @@ function displayNews(news) {
   news.forEach(article => {
     articles.push(generateArticle(article));
   });
-  newsDisplay.innerHTML = articles.join();
+  newsDisplay.innerHTML = articles.join("");
 }
 
 function generateArticle(article) {
   return `
-  <article id="article" class="article">
-    <a target="_blank" id="article__link" class="article__link" href="${
-      article.url
-    }">
-        <figure id="article__image" class="article__image"></figure>
-        <span id="article__date" class="article__date">${
-          article.publishedAt
-        }</span>
-        <header id="article__title" class="article__title">${
+  <article class="article">
+        <a target="_blank" class="article__link" href="${article.url}">
+            <figure class="article__image"></figure>
+            <span id="article__date" class="article__date">${
+              article.publishedAt
+            }</span>
+            <header class="article__title">${article.title}</header>
+            <span class="article__author">${article.author}</span>
+            <p class="article__description">${article.description}</p>
+        </a>
+        <button type="text" data-title="${
           article.title
-        }</header>
-        <span id="article__author" class="article__author">${
-          article.author
-        }</span>
-        <p class="article__description" id="article__description">${
-          article.description
-        }</p>
-    </a>
+        }" class="article__readlater">Read later</button>
     </article>
     `;
 }
@@ -72,5 +68,22 @@ function loadLiveNews(event) {
     });
 }
 
+newsDisplay.addEventListener("click", function(e) {
+  if (e.target.className === "article__readlater") {
+    const articleTitle = e.target.getAttribute("data-title");
+    liveNewsCollection.forEach(articleObj => {
+      return Object.keys(articleObj).find(articleKey => {
+        if (articleObj[articleKey] === articleTitle) {
+          readLater.push(articleObj);
+        }
+      });
+    });
+    e.target.setAttribute("disabled", "");
+  }
+});
+
 document.addEventListener("DOMContentLoaded", loadLiveNews);
 liveNewsButton.addEventListener("click", loadLiveNews);
+readLaterButton.addEventListener("click", function(e) {
+  displayNews(readLater);
+});
