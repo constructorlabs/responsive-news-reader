ad510f8216d60282b680d29a7458e5dfdd153283
Sheila HK <38942403+tigershk@users.noreply.github.com>
2018-06-17 18:14:36 +0100

    Added user input search. Responsive for mobile, tablet & laptop

--- news.js
+++ news.js
@@ -1,3 +1,6 @@
+// Code to display news from News API site. Displays latest headlines by default with ability to search by category and user-specified keyword. Responsive for mobile, tablet and laptop views.
+
+// Variable initialisation
 const myApiKey = "167aa74e22a045b58d8d8af7cb8effe8";
 const myCountry = "gb";
 let myCategory = "";
@@ -14,12 +17,14 @@ function append(parent, eleID) {
   return parent.appendChild(eleID);
 }
 
+// Main function to clear previous content and get new data
 function start(url) {
   const parent = query("main__content");
   parent.innerHTML = ` `;
   getTopStories(url);
 }
 
+// Create HTML to display info
 function displayNews(newsArray) {
   const parent = query("main__content");
   newsArray.forEach(newsItem => {
@@ -39,7 +44,7 @@ function displayNews(newsArray) {
   });
 }
 
-// Fetch actual start
+// Fetch data from API and populate newsArray with relevant items
 function getTopStories() {
   return fetch(url)
     .then(response => response.json())
@@ -54,7 +59,6 @@ function getTopStories() {
           publishDate: newsItem.publishedAt.substring(0, 10), //date
           storyLink: newsItem.url
         };
-
         newsData.publishDate = formatDate(newsData.publishDate);
         newsArray.push(newsData);
       });
@@ -62,6 +66,8 @@ function getTopStories() {
     })
     .catch(error => console.log(`Ruh-roh! ${error}`));
 }
+
+// Makes Dates look presentable
 function formatDate(date) {
   let myDate = new Date(date);
   const prettyDate = myDate.toDateString().substring(4, myDate.length);
@@ -69,9 +75,7 @@ function formatDate(date) {
   const month = myDate[0];
   const day = parseInt(myDate[1]);
   const year = myDate[2];
-
   let suffix = "";
-
   switch (day) {
     case 1:
     case 21:
@@ -89,10 +93,12 @@ function formatDate(date) {
       suffix = "th";
       break;
   }
-
   return `${day}${suffix} ${month} ${year}`;
 }
-// Category functionality -- footer
+
+// Listeners Below :
+
+// Search by Category functionality -- footer
 let footerCategory = query("footer__list");
 
 // Category - Add click event listener
@@ -103,7 +109,19 @@ footerCategory.addEventListener("click", function(event) {
   start(url);
 });
 
-// Category functionality -- header
+// User Search functionality -- header
+const searchStr = query("search__text");
+const searchForm = query("header__search");
+
+//Listener for Search Term
+searchForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  let mySearchTerm = `everything?q=${searchStr.value}`;
+  url = `https://newsapi.org/v2/${mySearchTerm}&apiKey=${myApiKey}`;
+  start(url);
+});
+
+// Search by Category functionality -- header (repeated in footer)
 let headerCategory = query("header__list");
 
 // Category - Add click event listener
@@ -114,10 +132,10 @@ headerCategory.addEventListener("click", function(event) {
   start(url);
 });
 
-// HOME button functionality
+// HEADLINE button functionality
 let home = query("main__nav");
 
-// HOME - Add click event listener
+// Headline - Add click event listener
 home.addEventListener("click", function(event) {
   event.preventDefault();
   url = `https://newsapi.org/v2/top-headlines?country=${myCountry}&apiKey=${myApiKey}`;
@@ -125,23 +143,23 @@ home.addEventListener("click", function(event) {
   start(url);
 });
 
-// Top button functionality
+// Scroll to Top button functionality
 let backToTop = query("footer__backtotop"),
   body = document.body,
   docElem = document.documentElement,
   offset = 100,
   scrollPos;
 
-// Top button - Add scroll event listener
+// Top button - Add scroll event listener to display button
 window.addEventListener("scroll", function(event) {
   scrollPos = body.scrollTop || docElem.scrollTop;
   backToTop.className = scrollPos > offset ? "visible" : "";
 });
 
-// Top button - Add click event listener
+// Top button - Add click event listener to execute scroll
 backToTop.addEventListener("click", function(event) {
   event.preventDefault();
   window.scrollTo(top);
 });
 
-start(url);
+start(url); // Initial page load :)
