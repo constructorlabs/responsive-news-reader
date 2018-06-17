1f0174ca795e6c9c5d107a3c50766ba92ded9184
etubom <way2young4u@hotmail.com>
2018-06-17 22:01:31 +0100

    Removed console logs

--- src/responsive-news.js
+++ src/responsive-news.js
@@ -18,7 +18,7 @@ function append(parent, el) {
 formEvent.addEventListener("submit", function(e) {
   e.preventDefault();
   const cityName = document.querySelector("#input");
-  //console.log(cityName);
+
   getNews(cityName.value);
 });
 
@@ -31,36 +31,27 @@ function getNews(cityName, createNode, append) {
   );
   fetch(apiCall)
     .then(function(response) {
-      //console.log(response.json());
       return response.json();
     })
     .then(function(data) {
       const articlesData = data;
-      // console.log(articlesData);
+
       let title = data.articles.map(function(item) {
-        //console.log(item.title);
         return (bodyContent.innerHTML += "<p>Title: " + item.title + "</p>");
       });
       let images = data.articles.map(function(item) {
-        console.log(item.urlToImage);
-        //let img = createNode("img");
-        //append(sideBarImg, img.images);
         return (sideBarImg.innerHTML += "<img src =" + item.urlToImage + ">");
       });
       let description = data.articles.map(function(item) {
-        //console.log(item.description);
         return (bodyContent.innerHTML += "<p>" + item.description + "</p>");
       });
       let newsSource = data.articles.map(function(item) {
-        //console.log(item.source.name);
         return (rightNav.innerHTML += "<p>" + item.source.name + "</p>");
       });
       let articleDate = data.articles.map(function(item) {
-        //console.log(item.publishedAt);
         return item.publishedAt;
       });
       let articleLink = data.articles.map(function(item) {
-        //console.log(item.url);
         return (bodyContent.innerHTML += `<p><a href =${item.url} > ${
           item.url
         }</a></p>`);
