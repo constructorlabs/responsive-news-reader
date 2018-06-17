f65180f6c84586b77a68f1ec9089923686cbf139
James McAllister <jrfmcallister@gmail.com>
2018-06-17 09:12:09 +0100

    added key to mapped items

--- src/articles/articaleList.js
+++ src/articles/articaleList.js
@@ -3,4 +3,4 @@ import React from "react";
 import { SingleArticle } from "./singelArticle";
 
 export const ArticleList = ({ allArticles }) =>
-  allArticles.map(data => <SingleArticle {...data} />);
+  allArticles.map((data, i) => <SingleArticle key={i} {...data} />);
