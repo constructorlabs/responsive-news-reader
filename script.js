2d40cffa6566744a7a1fae4c3b32f4e086bc0aea
jamal westfield <jamalwestfield@Jamals-MacBook-Pro.local>
2018-09-14 17:24:05 +0100

    starting trump articles

--- script.js
+++ script.js
@@ -1,49 +1,80 @@
-
 // Contains all the urls needed throughout the project
+function createNode(element) {
+    return document.createElement(element)
+}
+
+function append(parent, element) {
+    return parent.appendChild(element)
+}
+
+const rightArrow = document.querySelector(".next")
+rightArrow.addEventListener("click" ,function(){
+    pageNumber++;
+    console.log(`${pageNumber}`)
+    getTopStory(pageNumber);
+  
+})
+
+const leftArrow = document.querySelector(".prev")
+leftArrow.addEventListener("click", function(){
+    pageNumber--;
+    getTopStory(pageNumber);
+})
+
+
+function getTopStory(pageNumber){
+    const topStoryUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=1&apiKey=287554a05efe4127bd911a0a216a7b64&page=${pageNumber}`;
+    fetch(topStoryUrl)
+    .then(response => {
+        return response.json()
+    })
+    .then(body => {
+        console.log(body)
+        const topUsaNewsArticles = body.articles
+        return topUsaNewsArticles.map(article => {
+            div.innerHTML="";
+            const mainImg = createNode("img")
+            const header = createNode("h1")
+            const span = createNode("span")
+            const link = createNode("a")
+            const swiper = createNode("p")
+            header.textContent = article.title
+            mainImg.setAttribute("src", article.urlToImage)
+            span.innerHTML = `</p><span class ="image-span">${article.description}</span></p>`
+            link.innerHTML = `<p></p><a href=${article.url} target="_blank">Click here for full story<a/></p>`
+            swiper.innerHTML = `<p class="swipe">swipe for latest News</p>`
+            append(div, header)
+            append(div, mainImg)
+            append(div, swiper)
+            append(div, span)
+            append(div, link)
+        })
+    })
+}
+
 const urls = {
     apiKey: "287554a05efe4127bd911a0a216a7b64",
-    topBbcNews:"https://newsapi.org/v2/top-headlines -G \
+    topBbcNews: "https://newsapi.org/v2/top-headlines -G \
     -d sources=bbc-news \
     -d apiKey=287554a05efe4127bd911a0a216a7b64",
     topUsaNews: "https://newsapi.org/v2/top-headlines?country=us&apiKey=287554a05efe4127bd911a0a216a7b64",
     trumpNews: "https://newsapi.org/v2/top-headlines?country=us&pageSize=1&apiKey=287554a05efe4127bd911a0a216a7b64"
 }
-const ul = document.querySelector("#main")
 
-function createNode( element){
-    return document.createElement(element)
-}
+const div = document.querySelector("#main");
+let pageNumber = 1;
 
-function append(parent, element){
-    return parent.appendChild(element)
-}
+getTopStory(1); //reset page back page 1
 
 
-fetch(urls.trumpNews)
-.then(response => {
-    return response.json()
-})
-.then(body => {
-    console.log(body)
-    let trumpNewsArticles = body.articles
-    return trumpNewsArticles.map(article => {
-        let li = createNode("li")
-        mainImg = createNode("img")
-        header = createNode("h1")
-        span = createNode("span")
-        link = createNode("a")
-        swiper = createNode("p")
-        // img.src = article.urlToImage
-        header.textContent = article.title
-        mainImg.setAttribute("src", article.urlToImage)
-        span.innerHTML = `</p><span class ="image-span">${article.description}</span></p>`
-        link.innerHTML = `<p></p><a href=${article.url} target="_blank">Click here for full story<a/></p>`
-        swiper.innerHTML = `<p class="swipe">swipe for latest News</p>`
-        append(li,header)
-        append(li, mainImg)
-        append(li, swiper)
-        append(li, span)
-        append(li, link)
-        append(ul, li)
-    })
-})
+// const =
+// fetch(url)
+// .then(response => {
+//     return response.json()
+// })
+// .then(body => {
+//     console.log
+// })
+
+
+
