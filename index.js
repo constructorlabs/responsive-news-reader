dcf44689f053e67f0b65ac35f5d9fc529c434509
David Gridley <davidgridley21@gmail.com>
2018-09-14 15:33:02 +0100

    several changes added to html, css and js

---
+++ index.js
@@ -0,0 +1,54 @@
+//cd79c8caed5c47f4ae25ec30f7cca674 is API key
+
+const topHeadlinesUS = `https://newsapi.org/v2/top-headlines?country=us&apiKey=cd79c8caed5c47f4ae25ec30f7cca674`;
+const topHeadlinesUK = `https://newsapi.org/v2/top-headlines?country=gb&apiKey=cd79c8caed5c47f4ae25ec30f7cca674`;
+const topHeadlinesBrexit = `https://newsapi.org/v2/top-headlines?country=gb&q=brexit&apiKey=cd79c8caed5c47f4ae25ec30f7cca674`;
+
+
+function pullNewsObject(news) {
+    console.log(news);
+    news.articles.forEach(article => {
+        console.log(article);
+    })
+};
+
+
+function articleTemplate (article) {
+    let authorVar = "";
+    let sourceVar = "";
+    (article.author === null) ? authorVar = "" : authorVar = article.author;
+    (article.source.name === null) ? sourceVar = "" : sourceVar = article.source.name;
+    return `
+    <h2 class="article_title">${article.title}</h2>
+    <img class="article_image" src="${article.urlToImage}">
+    <p class="article_content">${article.content}</p>
+    <ul class="article_author_source_list">
+        <li class="author_item">${authorVar}</li>
+        <li class="source_item">${sourceVar}</li>
+    </ul>
+    <a class="article_link" href="${article.url}">Full article available here</a>
+    `
+}
+
+function createNewsContent(news) {
+    news.articles.forEach(item => {
+        if (item.content !== null && item.source.name !== "Bloomberg") {
+            const contentBody = document.querySelector(".content");
+            const articleBox = document.createElement("div");
+            articleBox.className = "article_box";
+            articleBox.innerHTML = articleTemplate(item) 
+            contentBody.append(articleBox);
+        }
+    })
+}
+
+
+fetch(topHeadlinesUK)
+    .then(response => {
+        console.log("promise has been resolved")
+        return response.json();
+    }) 
+    .then(body => {
+        // pullNewsObject(body);
+        createNewsContent(body);
+    });
