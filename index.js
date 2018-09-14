882ef3bd07f323d84ef8f8d813627592fe864733
Joe Lamb <joe@elevenplustwo.co.uk>
2018-09-14 15:27:08 +0100

    time elapsed since publication with conditional plurals

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 fetch(
-  'https://newsapi.org/v2/everything?q=bitcoin&from=2018-08-14&sortBy=publishedAt&apiKey=f29390555fbc483ba17e7ec1cb19af1a'
+  'https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=f29390555fbc483ba17e7ec1cb19af1a'
 )
   .then(response => {
     return response.json();
   })
   .then(data => {
-    addArticlesToNewsfeed(data.articles, 5);
+    addArticlesToNewsfeed(data.articles, 25);
   })
   .catch(err => {
     displayErrorToUser('Server failed to return data');
@@ -16,12 +16,11 @@ const createArticle = articleData => {
 
   const article = document.createElement('article');
   article.classList.add('news__article');
-  const elapsedTime = Math.floor(
-    (Date.now() - new Date(articleData.publishedAt).valueOf()) / 60000
-  );
+
+  const elapsedTime = getTimeSinceArticlePublication(articleData.publishedAt);
 
   article.innerHTML = `  <h2 class='news__headline'>${articleData.title}</h2>
-        <p class="news__date">Published ${elapsedTime} mins ago</p>
+        <p class="news__date">Published ${elapsedTime} ago</p>
                               <img class='news__image' src="${
                                 articleData.urlToImage
                               }" alt="${articleData.title}">
@@ -39,6 +38,32 @@ const createArticle = articleData => {
 
 const getArticles = (data, number) => {
   return data.filter(article => data.indexOf(article) <= number - 1);
+};
+
+const pluralUnits = (val, unit) => {
+  console.log(val);
+  return val >= 2 ? unit.replace(' ', 's ') : unit;
+};
+
+const getTimeSinceArticlePublication = date => {
+  let mins = Math.floor((Date.now() - new Date(date).valueOf()) / 60000);
+  let hours = mins / 60;
+  console.log(hours + 'hours');
+  let days = hours / 24;
+  console.log(days + 'days');
+  let elapsedTime = '';
+  if (days >= 1) {
+    elapsedTime += `${Math.floor(days)} ${pluralUnits(days, 'day ')}`;
+    hours = hours % 24;
+  }
+  if (hours >= 1) {
+    elapsedTime += `${Math.floor(hours)} ${pluralUnits(hours, 'hour ')}`;
+    mins = mins % 60;
+  }
+
+  elapsedTime += `${mins} ${pluralUnits(mins, 'min ')}`;
+
+  return elapsedTime;
 };
 
 const addArticlesToNewsfeed = (data, number) => {
