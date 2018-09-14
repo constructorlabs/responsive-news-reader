2eb0736434df44863f23de5402f1c92fc69f3d94
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-09-14 17:21:26 +0100

    finished @media (min-width: 768px)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const main = document.querySelector('.main');
+// fetch('https://newsapi.org/v2/top-headlines?apiKey=454852947b2747dabc52cf3ebcf4807c&country=au')
 
 fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=454852947b2747dabc52cf3ebcf4807c')
     .then(function (response) {
@@ -8,33 +9,43 @@ fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=454852947b27
     .then(function (body) {
         const articles = body.articles;
         articles.forEach((article) => {
+            const articleNews = document.createElement('div');
+            articleNews.className = 'main__articleNews';
+            const imageBox = document.createElement('div');
+            imageBox.className = 'main__articleNews__imageBox';
+            const textBox = document.createElement('div');
+            textBox.className = 'main__articleNews__textBox';
 
             const sectionMainTitle = document.createElement('section');
             console.log(article);
-            sectionMainTitle.className = 'main__title';
+            sectionMainTitle.className = 'main__articleNews__textBox__title';
             sectionMainTitle.textContent = article.title;
-            main.append(sectionMainTitle);
+            textBox.append(sectionMainTitle);
 
             const sectionMainDescription = document.createElement('section');
-            sectionMainDescription.className = 'main__description';
+            sectionMainDescription.className = 'main__articleNews__textBox__description';
             sectionMainDescription.textContent = article.description;
-            main.append(sectionMainDescription);
+            textBox.append(sectionMainDescription);
 
             const sectionMainImage = document.createElement('figure');
-            sectionMainImage.className = 'main__image';
+            sectionMainImage.className = 'main__articleNews__imageBox__image';
             sectionMainImage.style.backgroundImage = `url("${article.urlToImage}")`;
-            main.append(sectionMainImage);
+            imageBox.append(sectionMainImage);
 
             const sectionMainPublished = document.createElement('section');
-            sectionMainPublished.className = 'main__published';
+            sectionMainPublished.className = 'main__articleNews__textBox__published';
             sectionMainPublished.textContent = article.publishedAt;
-            main.append(sectionMainPublished);
+            textBox.append(sectionMainPublished);
 
             const sectionMainSource = document.createElement('section');
-            sectionMainSource.className = 'main__source';
+            sectionMainSource.className = 'main__articleNews__textBox__source';
             sectionMainSource.textContent = article.source.name;
-            main.append(sectionMainSource);
+            textBox.append(sectionMainSource);
 
+            articleNews.append(imageBox);
+            articleNews.append(textBox);
+
+            main.append(articleNews);
         })
 
 
