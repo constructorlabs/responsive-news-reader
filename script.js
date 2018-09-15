fe80608acda3379472c3f5adce65424db7f2ff68
jamal westfield <jamalwestfield@Jamals-MacBook-Pro.local>
2018-09-15 10:51:29 +0100

    navbar almost complete

--- script.js
+++ script.js
@@ -102,3 +102,11 @@ fetch(techUrl)
 
 
 
+
+
+var open_menu = document.getElementById("main-menu");
+var burger_menu = document.getElementById("burger_menu");
+burger_menu.addEventListener("click", function(){
+  burger_menu.classList.toggle("active-burger");
+  open_menu.classList.toggle("show-menu-mobile");
+});
