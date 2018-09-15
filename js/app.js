c680bb983605c018ab372fc5c4d827b8ba47854d
Mariusz Sygnowski <mariuszsygnowski@mariuszs-mbp.mynet>
2018-09-15 21:55:53 +0100

    changed from flex to css grid and few other small changes

--- js/app.js
+++ js/app.js
@@ -9,41 +9,47 @@ fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=454852947b27
     .then(function (body) {
         const articles = body.articles;
         articles.forEach((article) => {
+            console.log(article);
+
             const articleNews = document.createElement('div');
             articleNews.className = 'main__articleNews';
-            const imageBox = document.createElement('div');
-            imageBox.className = 'main__articleNews__imageBox';
-            const textBox = document.createElement('div');
-            textBox.className = 'main__articleNews__textBox';
 
-            const sectionMainTitle = document.createElement('section');
-            console.log(article);
-            sectionMainTitle.className = 'main__articleNews__textBox__title';
+            const sectionMainTitle = document.createElement('h2');
+            sectionMainTitle.className = 'main__articleNews__title';
             sectionMainTitle.textContent = article.title;
-            textBox.append(sectionMainTitle);
+            articleNews.append(sectionMainTitle);
 
             const sectionMainDescription = document.createElement('section');
-            sectionMainDescription.className = 'main__articleNews__textBox__description';
+            sectionMainDescription.className = 'main__articleNews__description';
             sectionMainDescription.textContent = article.description;
-            textBox.append(sectionMainDescription);
+            articleNews.append(sectionMainDescription);
+
+            const sectionMainContent = document.createElement('section');
+            sectionMainContent.className = 'main__articleNews__content';
+            let articleContent = article.content;
+            let newar = '';
+            if (articleContent !== null) { //checking if there something in article.content
+                newar = articleContent.replace(/ *\[[^)]*] */g, "");
+            } else {
+                console.log('no ok');
+            }
+            sectionMainContent.textContent = newar;
+            articleNews.append(sectionMainContent);
 
             const sectionMainImage = document.createElement('figure');
-            sectionMainImage.className = 'main__articleNews__imageBox__image';
+            sectionMainImage.className = 'main__articleNews__image';
             sectionMainImage.style.backgroundImage = `url("${article.urlToImage}")`;
-            imageBox.append(sectionMainImage);
+            articleNews.append(sectionMainImage);
 
             const sectionMainPublished = document.createElement('section');
-            sectionMainPublished.className = 'main__articleNews__textBox__published';
+            sectionMainPublished.className = 'main__articleNews__published';
             sectionMainPublished.textContent = article.publishedAt;
-            textBox.append(sectionMainPublished);
+            articleNews.append(sectionMainPublished);
 
             const sectionMainSource = document.createElement('section');
-            sectionMainSource.className = 'main__articleNews__textBox__source';
+            sectionMainSource.className = 'main__articleNews__source';
             sectionMainSource.textContent = article.source.name;
-            textBox.append(sectionMainSource);
-
-            articleNews.append(imageBox);
-            articleNews.append(textBox);
+            articleNews.append(sectionMainSource);
 
             main.append(articleNews);
         })
