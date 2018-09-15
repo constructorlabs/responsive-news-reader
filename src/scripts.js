e77414cf8a4da8db29b1b1ab40f075591e8e1836
DanGRT <d.geraghty06@gmail.com>
2018-09-15 15:53:04 +0100

    Added pagination function for searches only

--- src/scripts.js
+++ src/scripts.js
@@ -1,32 +1,54 @@
 // News API key: 9ed005ef4eb94baf913fce701c69972f
 
+//TODO: pagination, stylization, excludeDomains/blocklist
+
+//Object contains possible URLS for fetch requests
 const apiRequests = {
   usTop20:  'https://newsapi.org/v2/top-headlines?country=us&apiKey=9ed005ef4eb94baf913fce701c69972f',
 
   ukTop20: 'https://newsapi.org/v2/top-headlines?country=gb&apiKey=9ed005ef4eb94baf913fce701c69972f',
 
-  searchURL: function(userString, daterange){
+  displayTop20: true,
+
+//function takes search string, and if specified a date range
+  searchURL: function(userString, daterange, page){
+    this.displayTop20 = false
     if (daterange === null){
-      return `https://newsapi.org/v2/everything?q=${userString}&sortBy=popularity&apiKey=9ed005ef4eb94baf913fce701c69972f`
+      this.currentSearchURL = `https://newsapi.org/v2/everything?q=${userString}&page=1&sortBy=popularity&apiKey=9ed005ef4eb94baf913fce701c69972f`
+      return this.currentSearchURL
     }else{
-      return `https://newsapi.org/v2/everything?q=${userString}${daterange}&sortBy=popularity&apiKey=9ed005ef4eb94baf913fce701c69972f`
+      this.currentSearchURL = `https://newsapi.org/v2/everything?q=${userString}${daterange}&page=1&sortBy=popularity&apiKey=9ed005ef4eb94baf913fce701c69972f`
+      return this.currentSearchURL
     }
-  }
+  },
+
+  pageChange: function(inputPage){
+    const currentURL = this.currentSearchURL
+    const pageRegex = /page=(\d)/
+    this.currentPage = inputPage
+    return currentURL.replace(pageRegex, `page=${inputPage}`)
+  },
+
+  currentSearchURL: null,
+
+  currentPage: 1
 }
 
+
+//Object contains functions attached to buttons and page features
 const pageHandlers = {
   changeCountry: function(){
-                    console.log("test")
-                    const countryButtonElement = document.querySelector(".country-button")
-                    countryButtonElement.addEventListener("click", event => {
-                      contentElement.innerHTML = ""
-                      countryButtonElement.textContent === "Top UK Stories"
-                        ? ( countryButtonElement.textContent = "Top US Stories",
-                           fetchNews(apiRequests.ukTop20))
-                        : (countryButtonElement.textContent = "Top UK Stories",
-                           fetchNews(apiRequests.usTop20))
-                    })
-                  },
+    const countryButtonElement = document.querySelector(".country-button")
+      countryButtonElement.addEventListener("click", event => {
+        paginationElement.style.display = "none";
+        newsDisplayElement.innerHTML = ""
+        countryButtonElement.textContent === "Top UK Stories"
+          ? ( countryButtonElement.textContent = "Top US Stories",
+              fetchNews(apiRequests.ukTop20))
+          : (countryButtonElement.textContent = "Top UK Stories",
+              fetchNews(apiRequests.usTop20))
+      })
+    },
 
   search: function(){
     const searchFormElement = document.querySelector(".search-form")
@@ -36,12 +58,44 @@ const pageHandlers = {
       event.preventDefault();
       const dateRange = formatDate(dateRangeElement.value)
       const searchString = apiRequests.searchURL(searchTextElement.value, dateRange)
-      contentElement.innerHTML = ""
+      newsDisplayElement.innerHTML = ""
       searchTextElement.value = ""
+      paginationElement.style.display = "inline"
       fetchNews(searchString)
+      this.checkPage()
 
     })
+  },
+
+  checkPage: function(){
+    apiRequests.currentPage === 1
+      ? prevPageElement.disabled = true
+      : prevPageElement.disabled = false
+  },
+
+
+  paginationControl: function(){
+    nextPageElement.addEventListener("click", event => {
+      let currentPage = apiRequests.currentPage
+      currentPage ++
+      const requestedPage = apiRequests.pageChange(currentPage)
+      newsDisplayElement.innerHTML = ""
+      fetchNews(requestedPage)
+      this.checkPage()
+    })
+
+    prevPageElement.addEventListener("click", event => {
+      let currentPage = apiRequests.currentPage
+      currentPage --
+      const requestedPage = apiRequests.pageChange(currentPage)
+      newsDisplayElement.innerHTML = ""
+      fetchNews(requestedPage)
+      this.checkPage()
+    })
+
   }
+
+
 
 
 
@@ -49,10 +103,13 @@ const pageHandlers = {
 
 
 
-
+//creates each story panel from fetch response
 function createStoryPanel(article){
   const storyDivElement = document.createElement("div")
   storyDivElement.className = "story-div-element"
+
+  const headerElement = document.createElement("div")
+  headerElement.className = "story-header"
 
   const headlineElement = document.createElement("h4")
   headlineElement.className = "headline"
@@ -75,16 +132,18 @@ function createStoryPanel(article){
   linkElement.setAttribute("href", article.url)
   linkElement.textContent = "See full story"
 
-  storyDivElement.appendChild(headlineElement)
+  headerElement.appendChild(headlineElement)
+  headerElement.appendChild(storyImageElement)
+  storyDivElement.appendChild(headerElement)
   storyDivElement.appendChild(publicationNameElement)
   storyDivElement.appendChild(publicationDateElement)
-  storyDivElement.appendChild(storyImageElement)
+
   storyDivElement.appendChild(descriptionElement)
   storyDivElement.appendChild(linkElement)
-  contentElement.appendChild(storyDivElement)
+  newsDisplayElement.appendChild(storyDivElement)
 }
 
-
+//formats date for search
 function formatDate(monthsAgo){
   if ( monthsAgo != "all-time") {
     const newDate = new Date
@@ -95,7 +154,6 @@ function formatDate(monthsAgo){
     return null
   }
 }
-
 
 
 function fetchNews(apiAddress){
@@ -112,7 +170,12 @@ function fetchNews(apiAddress){
 
 
 const contentElement = document.querySelector(".content")
+const newsDisplayElement = document.querySelector(".news-display")
+const paginationElement = document.querySelector(".pagination")
+const nextPageElement = document.querySelector(".next-page")
+const prevPageElement = document.querySelector(".back-page")
 
 fetchNews(apiRequests.usTop20)
 pageHandlers.changeCountry()
 pageHandlers.search()
+pageHandlers.paginationControl()
