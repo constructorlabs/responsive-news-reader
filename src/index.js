4aeb5ec0eb2d5aa37e4d5a88f42cb2a634fe8bb1
Roland Levy <rolandjlevy@gmail.com>
2018-09-16 21:48:03 +0100

    search working OK with improved methods

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-/*
-======================================================
-SET UP 
-======================================================
-*/
+/* ======================================================
+DEFINE NODES */
 
+const parentNode = document.querySelector(".content__body");
 const articleNode = document.querySelector(".article__list");
+const sectionButtonsNode = document.querySelector(".content__section__buttons");
+const displayJSONNode = document.querySelector(".content__footer");
 const countryMenuNode = document.querySelector(".country");
 const categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
 
-/*
-======================================================
-FETCH DATA
-======================================================
-*/
+/* ======================================================
+FETCH DATA */
 
 const loadAPI = function (url) {
     fetch(url) 
@@ -27,32 +24,23 @@ const loadAPI = function (url) {
     });
 }
 
-/*
-======================================================
-FORMAT DATA FOR ARTICLES
-======================================================
-*/
+/* ======================================================
+FORMAT AND DISPLAY ARTICLES */
 
 const articleTemplate = article => {
-    // const keys = Object.keys(article);
-    // ["title", "description", "content"].forEach(function(key){
-    //     const convertedText = (article[key] !== null) ? highlightFoundWords(article[key]) : "";
-    //     article[key] = convertedText;
-    // });
     const convertedTitle = (article.title !== null) ? highlightFoundWords(article.title) : null;
     const title = (article.title !== null) ? `<li><strong>${convertedTitle}</strong></li>` : "";
     const convertedDescription = (article.description !== null) ? highlightFoundWords(article.description) : null;
     const description = (article.description !== null) ? `<li>${convertedDescription}</li>` : "";
-    const content = (article.content !== null) ? `<li>${article.content}</li>` : "";
-    // console.log(content);
+    // const content = (article.content !== null) ? `<li>${article.content}</li>` : "";
     const author = (article.author !== null) ? `${article.author}` : "";
     const source = (article.source.name !== null) ? `${article.source.name}` : "";
     const separator = (author && source) ? ", " : "";
     const publishedBy = `<li><cite>Published by: ${author}${separator}${source}</cite></li>`;
     const readMoreURL = (article.url !== null) ? `<li><a href="${article.url}" target="_blank">READ FULL STORY...</a></li>` : "";
-    const urlToImage = (article.urlToImage !== null) ? `<a href="${article.url}" target="_blank"><img src="${article.urlToImage}" class="article__image__src"></a>` : "";
+    // const urlToImage = (article.urlToImage !== null) ? `<a href="${article.url}" target="_blank"><img src="${article.urlToImage}" class="article__image__src"></a>` : "";
+    const urlToImage = (article.urlToImage !== null) ? `<a href="${article.url}" target="_blank" style="background-image:url(${article.urlToImage});" class="article__image__src"></a>` : "";
     const publishedAt = (article.publishedAt !== null) ? `Time: ${convertDate(article.publishedAt)}` : "";
-
     return `
     <div class="article__main">
         <div class="article__image">
@@ -75,9 +63,6 @@ const articleTemplate = article => {
     `;
 }
 
-/* //////////////////////////
-// DISPLAY DATA
-///////////////////////////*/
 
 function displayDataOnPage(body, url) {
     articleNode.innerHTML = "";
@@ -90,9 +75,8 @@ function displayDataOnPage(body, url) {
     displayJSONNode.innerHTML = `<a href="${url}" target="_blank">View JSON</a>`;
 }
 
-/* /////////////////////
-CREATE MENU AND BUTTONS
-//////////////////////*/
+/* /////////////////////////////////////////////////////////////////
+CREATE MENUS */
 
 const createCountriesMenu = function() {
     const menuNode = document.createElement("select");
@@ -105,15 +89,15 @@ const createCountriesMenu = function() {
     });
 }
 
-/* ///////////////////////////////////
-CREATE QUERIES
-////////////////////////////////////*/
+/* ////////////////////////////////////////////////////////////////////
+QUERYING */
 
 let bodyURL = "";
 const newsURL = "https://newsapi.org/v2/";
 const apiKey = "756ef978eb384d9cb3ecdab2d9bac0da";
 
 const queryAPI = function (type, country="", category="", search="") {
+    console.log("hi");
     let validCountry = country ? `country=${country}&` : "";
     let validCategory = category ? `category=${category}&` : "";
     let validSearch = search ? `q=${search}&` : "";
@@ -121,44 +105,36 @@ const queryAPI = function (type, country="", category="", search="") {
     return bodyURL;
 }
 
+const displayErrorToUser = error => articleNode.innerHTML = error;
 
-/* ///////////////////////////////////
-NAVIGATION SEARCH AND BUTTONS
-////////////////////////////////////*/
+createCountriesMenu();
+loadAPI(queryAPI("everything", "", "", "Donald+Trump"));
+
+/* /////////////////////////////////////////////////////////////////
+SEARCH FUNCTIONALITY */
 
 const navBar = document.querySelector(".content__nav");
 const navButton = document.querySelector(".header__nav__button");
 const navSearch = document.querySelector(".search");
 const searchFormNode = document.querySelector(".header__form");
-const displayJSONNode = document.querySelector(".content__footer");
+const messageNode = document.querySelector(".content___message");
+// const navSections = document.querySelector(".header__nav__sections a");
 
-/* ///////////////////////////////////
-SEARCH FUNCTIONALITY AND BUTTONS
-////////////////////////////////////*/
-
-// search function for search button and text field
-// const loadSearchResults = function(event) {
-//     const searchQuery = navSearch.value.split(" ").join("+");
-//     loadAPI(queryAPI("everything", "", "", searchQuery));
-// }
-console.log(navSearch);
-console.log(searchFormNode);
-
-// top nav search using 'click' event
-searchFormNode.addEventListener("submit", function(event) { 
+// search from top nav - click search button for results
+searchFormNode.addEventListener("submit", function(event){
     event.preventDefault();
-    const searchQuery = navSearch.value.split(" ").join("+");
+    let searchQuery = navSearch.value.split(" ").join("+");
     loadAPI(queryAPI("everything", "", "", searchQuery));
 });
 
-// top nav search using 'submit' event
-navSearch.addEventListener("submit", function(event) { 
+// load search results from 'submit' event
+navSearch.addEventListener("submit", function(event){
     event.preventDefault();
-    const searchQuery = navSearch.value.split(" ").join("+");
+    let searchQuery = navSearch.value.split(" ").join("+");
     loadAPI(queryAPI("everything", "", "", searchQuery));
 });
 
-// display the text "search results for:"
+// display message "search results for:"
 const displaySearchMessage = function(url) {
     if (url.indexOf("everything?") >= 0) {
         const searchNode = document.createElement("li");
@@ -169,32 +145,51 @@ const displaySearchMessage = function(url) {
     }
 }
 
-// use match function to convert the URL from search results into an array
+// convert search results URL into an array
 const convertedSearchArray = function (url) {
     let regex = /\A?q=[^&]*/g;
     const found = url.match(regex).toString();
     return found.split("=")[1].split("+");
 }
 
-// highlight all matched words from search
+// highlight words from search query
+// const highlightFoundWords = function (descriptionText) {
+//     if (bodyURL.indexOf("everything?") >= 0) {
+//         const searchResultsArray = convertedSearchArray(bodyURL);
+//         const updatedWords = descriptionText.split(" ").map(function(item){
+//             const found = searchResultsArray.find(function(searchItem) {
+//                 return searchItem === item;
+//             });
+//             return found !== undefined ? `<span class="highlighted">${found}</span>` : item;
+//         });
+//         return updatedWords.join(" ");
+//     } else {
+//         return descriptionText;
+//     }
+// }
+
 const highlightFoundWords = function (descriptionText) {
     if (bodyURL.indexOf("everything?") >= 0) {
         const searchResultsArray = convertedSearchArray(bodyURL);
-        const updatedWords = descriptionText.split(" ").map(function(item){
-            const found = searchResultsArray.find(function(searchItem) {
-                return searchItem === item;
-            });
-            return found !== undefined ? `<span class="highlighted">${found}</span>` : item;
-        });
+        // const updatedWords = descriptionText.split(" ").map(function(hay){
+        //     const found = searchResultsArray.find(needle => hay === needle);
+        //     return found ? `<span class="highlighted">${hay}</span>` : hay;
+        // });
+        const descriptionArray = descriptionText.split(" ")
+        const updatedWords = searchForMatches(descriptionArray, searchResultsArray);
         return updatedWords.join(" ");
     } else {
         return descriptionText;
     }
 }
 
-/* ///////////////////////////////////
-ARTICLES
-////////////////////////////////////*/
+const searchForMatches = function(haystackArray, needleArray) {
+    const output = haystackArray.map(function(hay){
+        const found = needleArray.find(needle => hay === needle);
+        return found ? `<span class="highlighted">${hay}</span>` : hay;
+    });
+    return output;
+}
 
 const convertDate = function (string) {
     const date = new Date(string);
@@ -202,15 +197,3 @@ const convertDate = function (string) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return `${formatTime(date.getHours())}:${formatTime(date.getMinutes())}, ${date.getDate()} ${ months[date.getMonth()]} ${date.getFullYear()}`
 }
-
-const displayErrorToUser = error => articleNode.innerHTML = error;
-createCountriesMenu();
-loadAPI(queryAPI("everything", "", "", "Donald+Trump"));
-
-/* ///////////////////////////////////
-DOCUMENTATION
-////////////////////////////////////*/
-// https://newsapi.org/sources
-// https://newsapi.org/docs
-// https://newsapi.org/docs/get-started
-// everything: https://newsapi.org/docs/endpoints/everything
