22fb927f2c0ab16973aee6e7feb820bec4cb4139
jamal westfield <jamalwestfield@Jamals-MacBook-Pro.local>
2018-09-16 11:07:14 +0100

    slider working for top story

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Contains all the urls needed throughout the project
+// functions
 function createNode(element) {
     return document.createElement(element)
 }
@@ -7,22 +7,9 @@ function append(parent, element) {
     return parent.appendChild(element)
 }
 
-const rightArrow = document.querySelector(".next")
-rightArrow.addEventListener("click" ,function(){
-    pageNumber++;
-    getTopStory(pageNumber);
-  
-})
-
-const leftArrow = document.querySelector(".prev")
-leftArrow.addEventListener("click", function(){
-    pageNumber--;
-    getTopStory(pageNumber);
-})
-
 
 function getTopStory(pageNumber){
-    const topStoryUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=1&apiKey=287554a05efe4127bd911a0a216a7b64&page=${pageNumber}`;
+    const topStoryUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=1&apiKey=288a1247b5a34780a74f689bdc7e1b83&page=${pageNumber}`;
     fetch(topStoryUrl)
     .then(response => {
         return response.json()
@@ -40,7 +27,7 @@ function getTopStory(pageNumber){
             header.textContent = article.title
             mainImg.setAttribute("src", article.urlToImage || 'http://placekitten.com/200/300')
             span.innerHTML = `</p><span class ="image-span">${article.description || `Nothing to display`}</span></p>`
-            link.innerHTML = `<p></p><a href=${article.url} target="_blank">Click here for full story<a/></p>`
+            link.innerHTML = `<p><a class ="btn-more" href=${article.url} target="_blank">Read More<a/></p>`
             published.innerHTML = `<p class="publishedAt">${article.published || `Posted Today`}</p>`
             swiper.innerHTML = `<p class="swipe">swipe for latest News</p>`
             append(topStoryDiv, header)
@@ -53,6 +40,13 @@ function getTopStory(pageNumber){
     })
 }
 
+
+function nextSlide(){
+    pageNumber++;
+    getTopStory(pageNumber);
+}
+
+let slideInterval = setInterval(nextSlide,3000);
 // const urls = {
 //     apiKey: "287554a05efe4127bd911a0a216a7b64",
 //     topBbcNews: "https://newsapi.org/v2/top-headlines -G \
@@ -62,6 +56,29 @@ function getTopStory(pageNumber){
 //     trumpNews: "https://newsapi.org/v2/top-headlines?country=us&pageSize=1&apiKey=287554a05efe4127bd911a0a216a7b64"
 // }
 
+
+// event listeners
+// const rightArrow = document.querySelector(".next")
+// rightArrow.addEventListener("click" ,function(){
+//     pageNumber++;
+//     getTopStory(pageNumber);
+  
+// })
+
+// const leftArrow = document.querySelector(".prev")
+// leftArrow.addEventListener("click", function(){
+//     pageNumber--;
+//     getTopStory(pageNumber);
+// })
+
+
+
+
+
+
+
+
+
 const topStoryDiv = document.querySelector("#main");
 let pageNumber = 1;
 getTopStory(1); //reset page back page 1
@@ -69,13 +86,13 @@ getTopStory(1); //reset page back page 1
 
 
 const techNewsDiv = document.querySelector(".techNews")
-const techUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=287554a05efe4127bd911a0a216a7b64"
+const techUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=288a1247b5a34780a74f689bdc7e1b83&${pageNumber}`
 fetch(techUrl)
 .then(response => {
     return response.json()
 })
 .then(body => {
-    // console.log(body)
+    console.log(body)
     const techNews = body.articles
         return techNews.map(article => {
             techNewsDiv.innerHTML="";
@@ -88,8 +105,8 @@ fetch(techUrl)
             header.textContent = article.title
             mainImg.setAttribute("src", article.urlToImage || 'http://placekitten.com/200/300')
             span.innerHTML = `</p><span class ="image-span">${article.description || `Nothing to display`}</span></p>`
-            link.innerHTML = `<p></p><a href=${article.url} target="_blank">Click here for full story<a/></p>`
-            published.innerHTML = `<p class="publishedAt">Posted ${new Date(article.published) || `Posted Today`}</p>`
+            link.innerHTML = `<p><a  class ="btn-more" href=${article.url} target="_blank">Read More<a/></p>`
+            published.innerHTML = `<p class="publishedAt">${article.published || `Posted Today`}</p>`
             swiper.innerHTML = `<p class="swipe">swipe for Latest Tech News</p>`
             append(techNewsDiv, header)
             append(techNewsDiv, mainImg)
@@ -102,11 +119,41 @@ fetch(techUrl)
 
 
 
+// search function
+// function newSearch(word){
+//     return fetch(`https://newsapi.org/v2/everything?q=${word}&apiKey=a62b82adc88947479824b7f88a2c44db`)
+//     .then(function(response){
+//      return response.json();
+//     })
+//     .then(function(data){
+//      createNews(data)
+//     })
+//     }
+
+// const submit = document.querySelector('.fa fa-search');
+// submit.addEventListener('click', function(event){
+//  if (searchInput.value !== "") {
+//    body.innerHTML = "";
+//    event.preventDefault()
+//  console.log(searchInput.value)
+//  newSearch(searchInput.value)
+//  }
+// })
 
 
-var open_menu = document.getElementById("main-menu");
-var burger_menu = document.getElementById("burger_menu");
+
+
+//menu toggle
+var open_menu = document.querySelector("#main-menu");
+var burger_menu = document.querySelector("#burger_menu");
 burger_menu.addEventListener("click", function(){
   burger_menu.classList.toggle("active-burger");
   open_menu.classList.toggle("show-menu-mobile");
 });
+
+
+
+
+
+
+
