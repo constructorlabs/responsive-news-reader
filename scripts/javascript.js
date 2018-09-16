86d7c06d2c6711e38b6922475f2d9492e8ad4477
Lemony <lemonystudio@gmail.com>
2018-09-16 10:40:07 +0100

    Functions wrangling

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,62 +1,31 @@
-
-/// GENERATE FETCH URL
-
-// news sources object
-  let publicationList = {
-    "daily-mail": false,
-    "mirror": false,
-    "metro": false,
-    "the-telegraph": false,
-    "financial-times": false,
-    "bbc-news": false,
-  }
-  let checkboxArray = document.querySelectorAll(".news--filter input");
-
-// function generateFetchURL(publicationList) {
-  
-  // baseUrl and default
-  const baseUrl = "https://newsapi.org/v2/top-headlines?apiKey=93238bcda39e4404852697d364b77971";
-  const defaultArray = ["bbc-news","daily-mail","mirror"];
-
-  // create an array from object using key values
-  let publicationArray = Object.keys(publicationList);
-  let filteredArray = publicationArray.filter(function(pub) {
-      return publicationList[pub] === true;
-  });
-
-  // compile fetch url
-  let defaultArrayUrl = `&sources=${defaultArray}`;
-  let filteredPublicationUrl = `&sources=${filteredArray}`;
-  let fullURL = "";
-  
-  checkboxArray.forEach(function (checkbox) {   
-    if (checkbox.checked === true) {
-      return fullURL = `${baseUrl}${filteredPublicationUrl}`;
-    }
-    else {
-      return fullURL = `${baseUrl}${defaultArrayUrl}`;
-    }
-  })
-// }
-
-
-//filter event 
-// checkboxArray.forEach(function(input) {
-//   input.addEventListener("change", function(generateFetchURL) {
-//       // new assigned value to match object key
-//       // console.log(event.target.value);
-//       // assign object value if checked is true
-//       if (event.target.checked === true) {
-//         publicationList[event.target.value] = true;
-//       }
-//       else {
-//         publicationList[event.target.value] = false;
-//       }
-//     })
-//   })
-
-/// DISPLAY DATA ON PAGE
+let checkboxArray = document.querySelectorAll(".news--filter input");
 const parentNode = document.querySelector(".news--area--feed");
+
+/* 
+--------------
+FETCH FUNCTION
+--------------
+*/
+const goFetch = function(fullURL) {
+
+  fetch(fullURL) // by default fetch makes a GET request
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(body) {
+      displayDataOnPage(body);
+    })
+    .catch(function(error) {
+      displayErrorToUser("Server failed to return data");
+      // need filter to NOT SHOW any news story with empty values. if any value is empty do no show
+    });
+}
+
+/*
+--------------------
+DISPLAY DATA ON PAGE
+--------------------
+*/
 
 function displayDataOnPage(newsStories) {
 
@@ -78,22 +47,77 @@ function displayDataOnPage(newsStories) {
     })  
 }
 
+/*
+------------------
+GENERATE FETCH URL
+------------------
+*/
+
+// news sources object
+let publicationList = {
+  "daily-mail": false,
+  "mirror": false,
+  "metro": false,
+  "the-telegraph": false,
+  "financial-times": false,
+  "bbc-news": false,
+}
+
+const generateFetchURL = function (publicationList) {
+  // baseUrl and default
+  const baseUrl = "https://newsapi.org/v2/top-headlines?apiKey=93238bcda39e4404852697d364b77971";
+  const defaultArray = ["bbc-news","daily-mail","mirror"];
+
+  // create an array from object using key values
+  let publicationArray = Object.keys(publicationList);
+  let filteredArray = publicationArray.filter(function(pub) {
+      return publicationList[pub] === true;
+  });
+
+  // compile fetch url
+  let defaultArrayUrl = `&sources=${defaultArray}`;
+  let filteredPublicationUrl = `&sources=${filteredArray}`;
+  let fullURL = "";
+  
+  checkboxArray.forEach(function (checkbox) {   
+    if (checkbox.checked === true) {
+      // RETURN VALUES - fullURL
+      return fullURL = `${baseUrl}${filteredPublicationUrl}`;
+    }
+    else {
+      return fullURL = `${baseUrl}${defaultArrayUrl}`;
+    }
+  })
+}
+
+/*
+----------------------
+CREATE CHECKBOX FILTER
+----------------------
+*/
+const createCheckboxFilter = function() {
+  checkboxArray.forEach(function(input) {
+      input.addEventListener("change", function(event) {
+          // new assigned value to match object key
+          // assign object value if checked is true
+          if (event.target.checked === true) {
+            publicationList[event.target.value] = true;
+          }
+          else {
+            publicationList[event.target.value] = false;
+          }
+          generateFetchURL(publicationList);
+        })
+  })
+}
+/* this function to run generateFetchURL() */
+createCheckboxFilter();
+
+/*
+---------------------
+ERROR HANDLER - TO DO
+---------------------
+*/
 function displayErrorToUser() {}
 
-// function goFetch(fullURL) {
-  fetch(fullURL) // by default fetch makes a GET request
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(body) {
-      //console.log(body);
-      displayDataOnPage(body);
-    })
-    .catch(function(error) {
-      displayErrorToUser("Server failed to return data");
 
-      // need filter to NOT SHOW any news story with empty values. if any value is empty do no show
-    });
-// }
-  
-// goFetch();
