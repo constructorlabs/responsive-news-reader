d1a97ceeb49a090f736ced2c6533075d037ab81f
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-09-16 17:01:06 +0100

    Completed with pagination.

--- src/fetchingData.js
+++ src/fetchingData.js
@@ -8,36 +8,131 @@
                                                                                 __/ |
                                                                                |___/ 
 */
-let url =
-  "https://newsapi.org/v2/top-headlines?q=trump&language=en&apiKey=2b19d03fc1cf4cd7b62ac5bb15c98827";
-
-const grabData = () => {
-  fetch(url)
-    .then(response => {
-      return response.json();
-      /*run animation on list items.*/
-    })
-    .then(body => {
-      /*
-        Send the 20(default) to another
-        function one at a time to populate our ul. 
-        */
-      for (let i = 0; i < body.articles.length; i++) {
-        createListElement(body.articles[i]);
-      }
-    });
-};
-//On page load.
-document.addEventListener("DOMContentLoaded", grabData());
-
+//Pagination
+let pageNum = 1;
+let todayGMT = new Date();
+//Get yesterday's date
+todayGMT.setDate(todayGMT.getDate() - 1);
+let convertedToISO = todayGMT.toISOString();
+let url = `https://newsapi.org/v2/everything?q=trump&language=en&from=${convertedToISO}&sortby=popularity&apiKey=2b19d03fc1cf4cd7b62ac5bb15c98827&page=${pageNum}`;
+/*
+        Send the 20(default) articles through a series of
+        functions one at a time to populate our ul. 
+        "uri" should be undefined the first time, but not subsequent times
+        for purposes of pagination.
+*/
+function grabData(uri) {
+  //Has something been passed into grabData()?
+  if (uri !== undefined) {
+    fetch(uri)
+      .then(response => {
+        return response.json();
+      })
+      .then(nextPageResults => {
+        console.log(uri);
+        console.log(nextPageResults);
+        passArticlesForCreation(nextPageResults);
+      });
+  } else {
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        passArticlesForCreation(body);
+      });
+  }
+}
 const refreshButton = document.querySelector(".refresh");
-refreshButton.addEventListener("click", function() {
+refreshButton.addEventListener("click", refreshFeed);
+
+const loadMoreButton = document.querySelector(".loadNext");
+loadMoreButton.addEventListener("click", updatedUrl);
+
+const topButton = document.querySelector(".up");
+topButton.addEventListener("click", backUp);
+
+const navList = document.querySelector(".options");
+navList.addEventListener("click", function(event) {
+  if (event.target.id !== "trump") {
+    let country = event.target.id;
+    let page = 1;
+    let period = convertedToISO;
+    let newFeedUrl = `https://newsapi.org/v2/top-headlines?q=trump&language=en&from=${period}&sortby=popularity&country=${country}&apiKey=2b19d03fc1cf4cd7b62ac5bb15c98827&page=${page}`;
+    const feed = document.querySelector(".articles__wrapper");
+    feed.innerHTML = "";
+    grabData(newFeedUrl);
+  } else {
+    trumpify();
+  }
+});
+
+function trumpify() {
+  console.log("present");
+  const articles = document.querySelectorAll(".article__whole");
+  console.log(articles[1].childNodes);
+  articles.forEach(article => {
+    article.childNodes[0].childNodes[0].textContent = randomPraise();
+    article.childNodes[0].childNodes[0].style.backgroundColor = "#fff";
+    article.style.backgroundImage = "url('images/flag.png')";
+  });
+}
+
+function randomPraise() {
+  let praise = [
+    "Trump did nothing wrong.",
+    "Global superstar president Trump turns down nobel prize.",
+    "World peace declared under the greatest man to ever live, Donald Trump.",
+    "American masses call for all faces on Mt. Rushmore be replaced with Trump's dynamic visage.",
+    "The President has saved the lives of five local firemen.",
+    "Nine people confirmed better off under President Trump.",
+    "The President of the United States is a stand up dude.",
+    "'Make America Great Again' to be title of Spielberg's new biopic.",
+    "Quentin Tarantino 'okay, for a Jew' declares Trump magnanamously.",
+    "Trump to fight for 'bigger' womens rights.",
+    "New study shows: If you don't like the President... you're a communist.",
+    "Oh say can you see, that Trump is the greatest.",
+    "Fox News declares fealty to the legend himself.",
+    "Melania's US birth certificate miraculously found in the hands of Administration intern.",
+    "Tariffs - too short a word for lexicographer Trump. Should be Tariffics.",
+    "'Organge is the new black. You're all racist.' Declares Trump.",
+    "President orders new mandates for pronunciation: 'The Jina Ammendment'.",
+    "Lord of the Rings to be reshot with the President.",
+    "Trump overheard:'Kim Jong Un... sounds delicious, when's dinner?'",
+    "Hillary Clinton to be forcibly renamed Shrek, under Federal Law.",
+    "The USA to be henceforth referred to in full, according to President Donald J Trump, Great Proclaimer, Holy Unifier and Friend to Small Woodland Creatures."
+  ];
+  let rand = praise[Math.floor(Math.random() * praise.length)];
+  return rand;
+}
+
+function backUp() {
+  window.scroll(0, 0);
+}
+
+function refreshFeed() {
   //Clear the feed.
-  const feed = document.querySelector("articles__wrapper");
+  const feed = document.querySelector(".articles__wrapper");
   feed.innerHTML = "";
   //Fill the feed.
+
   grabData();
-});
+}
+
+function updatedUrl() {
+  pageNum++;
+  let period = convertedToISO;
+  let pageChange = pageNum + 1;
+  let updated = `https://newsapi.org/v2/everything?q=trump&language=en&from=${period}&sortby=popularity&apiKey=2b19d03fc1cf4cd7b62ac5bb15c98827&page=${pageChange}`;
+  grabData(updated);
+}
+
+function passArticlesForCreation(jsonData) {
+  for (let i = 0; i < jsonData.articles.length; i++) {
+    createListElement(jsonData.articles[i]);
+    //console.log(jsonData.articles[i]);
+  }
+}
 
 //As they pass in, create list elements from them.
 //Call populateNewsFeed on each one.
@@ -73,7 +168,11 @@ function createListElement(articleObject) {
   datePubWrapper.className = "date__pub";
 
   const articleDate = document.createElement("p");
-  articleDate.textContent = articleObject.publishedAt;
+  const pubDate = new Date(articleObject.publishedAt);
+  const ammendedDate = pubDate
+    .toString()
+    .replace("GMT+0100 (British Summer Time)", "");
+  articleDate.textContent = ammendedDate;
   articleDate.className = "article__date";
 
   const articlePub = document.createElement("p");
@@ -97,13 +196,20 @@ function createListElement(articleObject) {
 // Populate the created list elements:
 function populateNewsFeed(article) {
   feed = document.querySelector(".articles__wrapper");
-  feed.prepend(article);
+  if (pageNum === 1) {
+    feed.prepend(article);
+  } else {
+    feed.appendChild(article);
+  }
 }
 
-const refineFeed = document.querySelector(".refine");
-refineFeed.addEventListener("click", function() {
-  const refineList = document.querySelector(".dropdown__list");
-});
+//PROGRAM START
+document.addEventListener("DOMContentLoaded", grabData());
+document.querySelector(".options").style.cursor = "pointer";
+// const refineFeed = document.querySelector(".refine");
+// refineFeed.addEventListener("click", function() {
+//   const refineList = document.querySelector(".dropdown__list");
+// });
 
 /*  _____ _                     _      _        
   / ____(_)                   (_)    | |       
@@ -115,8 +221,8 @@ refineFeed.addEventListener("click", function() {
                                                 */
 
 //Causes putin to pop up when the 'Grabber' logo is hovered.
-const home = document.querySelector(".home");
-home.addEventListener("mouseover", function() {
-  const putin = document.querySelector(".oi__cheeky");
-  putin.style.display = "inline";
-});
+// const home = document.querySelector(".home");
+// home.addEventListener("mouseover", function() {
+//   const putin = document.querySelector(".oi__cheeky");
+//   putin.style.display = "inline";
+// });
