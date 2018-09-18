fd38c9d0bb79268619f5cee834389d7ff9adf81c
Kate <mhq0612@163.com>
2018-09-18 17:50:17 +0100

    fetch fixed; code tidied up

--- newsReader.js
+++ newsReader.js
@@ -4,86 +4,111 @@ const articleList=document.querySelector(".articleList");
 const nextPageButton=document.querySelector(".nextPage");
 const searchForm=document.querySelector(".searchForm");
 const keyword=document.querySelector(".keyword");
+const body=document.querySelector("body");
 let p=1;
 
-let urlBase = 'https://newsapi.org/v2/'
+const urlBase = 'https://newsapi.org/v2/'
 let queries = 'top-headlines?' +
-              'country=gb&'
+'country=gb&'
 let page = `page=${p}&`;
-let pageSize = 'pageSize=5&';
-let apiKey = 'apiKey=84e1f3efc2a148dca439f1b5ad3cd201';
+const pageSize = 'pageSize=5&';
+const apiKey = 'apiKey=84e1f3efc2a148dca439f1b5ad3cd201';
 let url = urlBase + queries + pageSize + page + apiKey;
-
-let req = new Request(url);
-fetch(req)
-    .then(response => {
-
-      return response.json()
-    })
-    .then(response => {
-      console.log(response);
-      let i=1
-        response.articles.forEach(function(item){
-          const newsDetail=document.createElement("ul");
-          newsDetail.classList.add("onePieceofNews");
-          newsDetail.setAttribute("id",i)
-          if(item.description!==null){
-          newsDetail.innerHTML=`<li class=title >${item.title}</li>
-                                <img src=${item.urlToImage} width=100% alt="image">
-                                <li class=description>${item.description}</li>
-                                <li class=source>${item.source.name}</li>
-                                <li class=date>${item.publishedAt}</li>
-                                <a href=${item.url}>Read more</a>
-                                `;
-
-          articleList.appendChild(newsDetail);
-          i++;
-        }
-          return;
-        })
-
-    })
-    .catch(error => console.log(error));
+const newsDetail=document.createElement("ul");
+newsDetail.classList.add("onePieceofNews");
+articleList.appendChild(newsDetail);
 
 
-nextPageButton.onclick=function(event)  {
-    p++;
-    page = `page=${p}&`
-    url = urlBase + queries + pageSize + page + apiKey;
-    console.log("url: ", url);
-
-req = new Request(url);
-fetch(req)
-    .then(response => {
-      return response.json()
-    })
-    .then(response => {
-      console.log(response);
-
-      i=1;
-        response.articles.forEach(function(item){
-
-          if(item.description!==null){
-          newsDetail=document.getElementById(`${i}`)
-          newsDetail.innerHTML=`<li class=title >${item.title}</li>
-                                <img src=${item.urlToImage} width=100% alt="image">
-                                <li class=description>${item.description}</li>
-                                <li class=source>${item.source.name}</li>
-                                <li class=date>${item.publishedAt}</li>
-                                <a href=${item.url}>Read more</a>
-                                `;
+function getNews(piece){
+  const newsContent=document.createElement("li")
+  if(piece.description!==null){
+    newsContent.innerHTML=`
+    <h3 class="title">${piece.title}</h3>
+    <img src=${piece.urlToImage} width=100% alt="image">
+    <p class="description">${piece.description}</p>
+    <p class="source">${piece.source.name}</p>
+    <p class="date">${piece.publishedAt}</p>
+    <a href=${piece.url}>Read more</a>
+    `;
+    newsDetail.appendChild(newsContent);
 
 
-
-          i++;
-          document.documentElement.scrollTop = 0;
-        }
-          return;
-        })
-
-    })
-    .catch(error => console.log(error));
   }
+}
+
+
+
+function fetchNews(){
+
+  return fetch(url)
+      .then(response =>  response.json())
+      .then(response => {
+        newsDetail.innerHTML="";
+          response.articles.forEach(piece => getNews(piece))
+          })
+      .catch(error => console.log(error));
+
+}
+
+fetchNews();
+
+function fetchNewsNextPage(pageNumber){
+  page = `page=${pageNumber}&`
+  url = urlBase + queries + pageSize + page + apiKey;
+  return fetch(url)
+  .then(response => response.json())
+  .then(response => {
+    newsDetail.innerHTML="";
+    return response.articles.forEach(piece => getNews(piece))
+  })
+  .catch(error => console.log(error));
+}
+
+body.addEventListener("click", event => {
+
+  if(event.target.matches(".nextPage")){
+    p++;
+    fetchNewsNextPage(p)
+  }
+})
+// nextPageButton.onclick=function(event)  {
+//     p++;
+//     page = `page=${p}&`
+//     url = urlBase + queries + pageSize + page + apiKey;
+//     console.log("url: ", url);
+//
+// req = new Request(url);
+// fetch(req)
+//     .then(response => {
+//       return response.json()
+//     })
+    // .then(response => {
+    //   console.log(response);
+    //
+    //   i=1;
+    //     response.articles.forEach(function(item){
+    //
+    //       if(item.description!==null){
+    //       newsDetail=document.getElementById(`${i}`)
+    //       newsDetail.innerHTML=`<li class=title >${item.title}</li>
+    //                             <img src=${item.urlToImage} width=100% alt="image">
+    //                             <li class=description>${item.description}</li>
+    //                             <li class=source>${item.source.name}</li>
+    //                             <li class=date>${item.publishedAt}</li>
+    //                             <a href=${item.url}>Read more</a>
+    //                             `;
+    //
+    //
+    //
+    //       i++;
+    //       document.documentElement.scrollTop = 0;
+    //     }
+    //       return;
+    //     })
+    //
+    // })
+  //   .catch(error => console.log(error));
+  // }
 
 searchForm.addEventListener('submit', function(event){
     event.preventDefault();
